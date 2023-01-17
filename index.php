5f1d848367c40d54c7aae20c7b3cf9b0ee7dc5e7
zahratuljannah <zahratuljannah358901@gmail.com>
2023-01-17 15:05:42 +0700

    update 17/01

--- index.php
+++ index.php
@@ -2,47 +2,50 @@
     <head>
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/glyphicons.css">
     </head>
     <body>
-        <div class="wrapper">
-            <div class="container main">
-                <div class="row">
-                    <div class="col-md-6 side-image">
-                        <!-- image -->
-                        <img src="gambar/login.svg" alt="">
-                        <div class="text">
-                            <p>join the community of developers <i>start</i></p>
-                        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 my-3 pt-5 right">
+                    <h3 class="text-center">Welcome back</h3>
+                    <h6 class="text-center small">Continue with Google or enter your details.</h6>
+                    <div class="d-grid pt-2">
+                        <button type="button" class="btn btn-outline-dark">
+                            <img src="gambar/google.png" alt=""> 
+                            Log in with Google
+                        </button>
                     </div>
-                    <div class="col-md-6 right">
-                        <div class="input-box">
-                            <header>welcome back</header>
-                            <div class="input-field">
-                                <input type="email" class="input" id="email"  name="email" required autocomplete="off">
-                                <label for="email" >Email</label>
-                            </div>
-                            <div class="input-field">
-                                <input type="password" class="input" id="pass" name="pass" required>
-                                <label for="pass" >Passsword</label>
-                            </div>
-                            
-                            <div class="input-field">
-                                <input type="submit" class="submit" value="Log In">
-                            </div>
-                                
-                            <div class="login">
-                                <span>Don't have an account? <a href="">sign up</a></span>
-                            </div>
-                                
-                                
-                            
+                    <p class="text-center pt-1">or</p>
+                    <form>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
                         </div>
-
-                    </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password">
+                        </div>
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="check">
+                            <label class="form-check-label" for="check">Remember for 30 days <a href="forgot password"></a></label>
+                            <a href="" class="text-decoration-right">forgot password</a>
+                        </div>
+                        <div class="d-grid">
+                            <button type="submit" class="btn btn-dark">Log in</button>
+                        </div>
+                    </form>   
+                    <div class="text-center form">
+                        <span>Don't have an account? 
+                            <a href="">Sign up for free</a>
+                        </span>  
+                    </div>                 
+                </div>
+                <div class="col-md-8 image p-0">
+                    <img class="img-fluid" src="gambar/bg.jpg" alt="">
                 </div>
             </div>
+            
         </div>
-
-
     </body>
 </html>
