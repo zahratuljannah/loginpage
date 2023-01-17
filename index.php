fa159b3ce3b0f40255dd9d84b9c96ac2820ca075
zahratuljannah <zahratuljannah358901@gmail.com>
2023-01-17 09:06:55 +0700

    commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="container main">
+                <div class="row">
+                    <div class="col-md-6 side-image">
+                        <!-- image -->
+                        <img src="gambar/login.svg" alt="">
+                        <div class="text">
+                            <p>join the community of developers <i>start</i></p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 right">
+                        <div class="input-box">
+                            <header>welcome back</header>
+                            <div class="input-field">
+                                <input type="email" class="input" id="email"  name="email" required autocomplete="off">
+                                <label for="email" >Email</label>
+                            </div>
+                            <div class="input-field">
+                                <input type="password" class="input" id="pass" name="pass" required>
+                                <label for="pass" >Passsword</label>
+                            </div>
+                            
+                            <div class="input-field">
+                                <input type="submit" class="submit" value="Log In">
+                            </div>
+                                
+                            <div class="login">
+                                <span>Don't have an account? <a href="">sign up</a></span>
+                            </div>
+                                
+                                
+                            
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </body>
+</html>
