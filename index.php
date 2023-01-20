d5d9c66d2ea7fce1eae3cb1b337aa73700754f31
zahratuljannah <zahratuljannah358901@gmail.com>
2023-01-20 08:51:14 +0700

    css1

--- index.php
+++ index.php
@@ -2,47 +2,38 @@
     <head>
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/glyphicons.css">
     </head>
     <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-4 my-3 pt-5 right">
+        <div class="container p-5" >
+            <div class="row shadow-lg rounded-4">
+                <div class="col-md-7 py-4 px-5 order-2 left">
                     <h3 class="text-center">Welcome back</h3>
-                    <h6 class="text-center small">Continue with Google or enter your details.</h6>
-                    <div class="d-grid pt-2">
-                        <button type="button" class="btn btn-outline-dark">
-                            <img src="gambar/google.png" alt=""> 
-                            Log in with Google
-                        </button>
-                    </div>
-                    <p class="text-center pt-1">or</p>
-                    <form>
+                    <h6 class="text-center small">please enter your details.</h6>
+                    <form class="pt-5 px-5">
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                            <label for="email" class="form-label fs-6">Email</label>
+                            <input type="email" class="form-control border-0 border-bottom fs-6" id="email" aria-describedby="emailHelp">
                         </div>
                         <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password">
+                            <label for="password" class="form-label fs-6">Password</label>
+                            <input type="password" class="form-control border-0 border-bottom fs-6" id="password">
                         </div>
                         <div class="mb-3 form-check">
                             <input type="checkbox" class="form-check-input" id="check">
-                            <label class="form-check-label" for="check">Remember for 30 days <a href="forgot password"></a></label>
-                            <a href="" class="text-decoration-right">forgot password</a>
+                            <label class="form-check-label small" for="check">Remember for 30 days</label>
                         </div>
                         <div class="d-grid">
-                            <button type="submit" class="btn btn-dark">Log in</button>
+                            <button type="submit" class="btn btn-dark fs-6">Log in</button>
                         </div>
                     </form>   
-                    <div class="text-center form">
+                    <div class="text-center ms-1 form small">
                         <span>Don't have an account? 
-                            <a href="">Sign up for free</a>
+                            <a href="" class="text-decoration-none ms-0 text-dark">Sign up for free</a>
                         </span>  
                     </div>                 
                 </div>
-                <div class="col-md-8 image p-0">
-                    <img class="img-fluid" src="gambar/bg.jpg" alt="">
+                <div class="col-md-5 p-0 order-1 image">
+                    <img class="w-100 h-100 rounded-start-4" src="gambar/bg.jpg" alt="">
                 </div>
             </div>
             
